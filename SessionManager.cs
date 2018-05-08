91d8c2574ce59f84ca9d3aa4a5c04c5d41bb0673
s.zorin <s.zorin@logexpert.ru>
2018-05-08 11:02:47 +0300

    add session manager

---
+++ SessionManager.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using WialonHostingSharp.Auth;
+
+namespace WialonHostingSharp
+{
+    public static class SessionManager
+    {
+        public static async Task<Session> Login(string host, string token, bool ssl = false)
+        {
+            var connection = new Connection(host);
+            connection.Ssl = ssl;
+            var logReq = new LoginRequest(connection, token);
+
+            var resp = await logReq.GetResponse();
+            var session = new Session(host, resp.SessionId);
+            session.Ssl = ssl;
+            return session;
+        }
+
+        public static Task<LogOutResult> Logout(Session session)
+        {
+            var ltReq = new LogOutRequest(session);
+            return ltReq.GetResponse();
+        }
+    }
+}
