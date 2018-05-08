728139be1ba0a1a47e4fbcc84b5f5f4ccb13e6ac
s.zorin <s.zorin@logexpert.ru>
2018-05-08 11:53:49 +0300

    add msg service

---
+++ Messages/MessageService.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using System;
+using WialonHostingSharp;
+
+namespace WialonHostingSharp.Messages
+{
+    public sealed class MessageService
+    {
+        public readonly Session session;
+
+        public MessageService(Session session)
+        {
+            this.session = session;
+        }
+
+        public async Task<MessageData[]> GetMessageDatas(int id, DateTime begin, DateTime end,
+            long count = 0xffffffff, long flags = 0)
+        {
+            var param = new LoadMessagesParams
+            {
+                ItemId = id,
+                Flags = flags,
+                BeginDate = begin,
+                EndDate = end,
+                Count = count
+            };
+
+            var req = new LoadMessagesRequest<MessageData>(session, param);
+            var result = await req.GetResponse();
+            return result.Messages;
+        }
+    }
+}
