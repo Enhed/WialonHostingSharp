8279f4481712a4d36062ee9d4df928f9165bf95a
s.zorin <s.zorin@logexpert.ru>
2018-05-08 11:13:59 +0300

    move to search namespace

--- Search/GroupObjects.cs
+++ Search/GroupObjects.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using WialonHostingSharp;
 
-namespace WialonHostingSharp
+namespace WialonHostingSharp.Search
 {
     public sealed class GroupObjects : WlnObject
     {
--- Search/GroupObjectsExtension.cs
+++ Search/GroupObjectsExtension.cs
@@ -1,7 +1,7 @@
 using System.Threading.Tasks;
 using WialonHostingSharp.Search;
 
-namespace WialonHostingSharp
+namespace WialonHostingSharp.Search
 {
     public static class GroupObjectsExtension
     {
