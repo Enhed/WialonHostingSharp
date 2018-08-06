a1583db49db5b22caec6e2f2f6ee69b36ad08e82
s.zorin <s.zorin@logexpert.ru>
2018-08-06 15:26:03 +0300

    add flags

--- Extensions/GroupObjectsExtension.cs
+++ Extensions/GroupObjectsExtension.cs
@@ -5,10 +5,10 @@ namespace WialonHostingSharp.Extensions
 {
     public static class GroupObjectsExtension
     {
-        public static Task<WlnObject[]> GetObjects(this GroupObjects obj, SearchService ss)
+        public static Task<WlnObject[]> GetObjects(this GroupObjects obj, SearchService ss, long flags = 1)
         {
             if(obj.ObjectIds?.Length == null) return null;
-            return ss.GetObjects(string.Join(",",obj.ObjectIds), PropertyElement.sys_id);
+            return ss.GetObjects(string.Join(",",obj.ObjectIds), PropertyElement.sys_id, flags);
         }
     }
 }
