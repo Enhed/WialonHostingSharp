8210c378f2a70ac87015ca081142369303faaac1
s.zorin <s.zorin@logexpert.ru>
2018-08-07 12:09:38 +0300

    add extend methods

--- Messages/MessageData.cs
+++ Messages/MessageData.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace WialonHostingSharp.Messages
@@ -15,6 +16,24 @@ namespace WialonHostingSharp.Messages
         public uint DataOut;
 
         [JsonProperty("p")]
-        public Dictionary<string, double> Parameters;
+        public Dictionary<string, string> Parameters;
+
+        public GalileSkyDataMessage ToGalileoSky() => new GalileSkyDataMessage { DataMessage = this };
+    }
+
+    public sealed class GalileSkyDataMessage
+    {
+        public DataMessage DataMessage;
+
+        public bool IsGpsAntennaOff => (DataMessage.DataOut & (1 << 22)) > 0;
+    }
+
+    public static class DataMessageExtension
+    {
+        public static IEnumerable<DataMessage> GetInvalids(this IEnumerable<DataMessage> source)
+            => source.Where(dm => !dm.Position?.IsValid ?? true);
+
+        public static IEnumerable<DataMessage> GetValids(this IEnumerable<DataMessage> source)
+            => source.Where(dm => dm.Position?.IsValid ?? false);
     }
 }
