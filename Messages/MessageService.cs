4be1d643e311d231a74bf80efc1d722a4d62e4f0
s.zorin <s.zorin@logexpert.ru>
2018-08-08 08:50:27 +0300

    add methods delete

--- Messages/MessageService.cs
+++ Messages/MessageService.cs
@@ -1,6 +1,11 @@
 using System.Threading.Tasks;
 using System;
 using WialonHostingSharp;
+using WialonHostingSharp.Http;
+using Newtonsoft.Json;
+using SharpExtension.For;
+using System.Linq;
+using SharpExtension;
 
 namespace WialonHostingSharp.Messages
 {
@@ -36,6 +41,77 @@ namespace WialonHostingSharp.Messages
             var req = new LoadMessagesRequest<T>(session, param);
             var result = await req.GetResponse();
             return result.Messages;
+        }
+
+        public Task<(bool Success, string Response)> Delete(uint index)
+        {
+            return new DeleteMessageRequest
+            (
+                session,
+                new DeleteMessageRequest.Params{ MessageIndex = index }
+            ).GetResponse();
+        }
+
+        public async Task Delete(Message[] messages, int maxIter = 10 )
+        {
+            var count = messages.Length / maxIter;
+            var index = 0;
+            
+            for(var i = 0; i <= count; ++i)
+            {
+                var iend = index + maxIter;
+
+                var asd = new uint[10];
+                
+                var tasks = (index, Math.Min(messages.Length, iend)).ForGet(x => x).Select( x => Delete((uint)x) );
+                var resps = await Task.WhenAll(tasks);
+
+                var fullsuccess = resps.All(x => x.Success)
+#if !DEBUG
+                    ;
+#else
+                    .Do(x => Console.WriteLine($"Success delete {index} - {iend}"));
+#endif
+
+                if(!fullsuccess)
+                    throw new Exception( $"success x[{resps.Count(x => x.Success)}] {resps.First(x => !x.Success).Response}" );
+
+                index = iend;
+            }
+        }
+
+        public async Task Replace(int index, Message msg)
+        {
+            
+        }
+
+        public async Task Delete(int id, DateTime begin, DateTime end, int maxIter = 10)
+        {
+            var messages = await GetDataMessages(id, begin, end);
+            await Delete(messages, maxIter);
+        }
+    }
+
+    public sealed class DeleteMessageRequest : Request<(bool Success, string Response)>
+    {
+        public const string SuccessResponse = "{}";
+
+        public DeleteMessageRequest(Session connection, Params parameters)
+            : base(connection, parameters)
+        {
+        }
+
+        public override string Method => "messages/delete_message";
+
+        public sealed class Params : RequestParams
+        {
+            [JsonProperty("msgIndex")]
+            public uint MessageIndex;
+        }
+
+        protected override (bool Success, string Response) Convert(string source)
+        {
+            return ( source.Trim() == SuccessResponse, source );
         }
     }
 }
